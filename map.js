c92881b9e5e44f3d0a287e82483f80c129ad4b2e
dairvin9 <dairvin9@tamu.edu>
2016-06-08 18:08:32 -0500

    adding and testing city scout

---
+++ map.js
@@ -0,0 +1,8 @@
+/* Uses the google maps api */
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
